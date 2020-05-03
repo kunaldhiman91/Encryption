834bbc7f9b71dccd857fa75b08bf2e57ff1e25a2
Kunal Kumar <kunaldhiman91@gmail.com>
2020-05-02 23:03:33 -0400

    Initial Commit

---
+++ Encryption/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Encryption
+//
+//  Created by Kunal Kumar on 2020-05-02.
+//  Copyright © 2020 KD inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
